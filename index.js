b93dfc661b8f2fcb1547a7bf48ee583ed87e7a3b
ameerkabir <ameerkabir2@gmail.com>
2018-09-08 00:19:25 +0100

    added payload handling

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 //Primary file for the api
 
 //Dependencies
-const http = require('http')
-const url =  require('url')
+const http = require("http");
+const url = require("url");
+const { StringDecoder } = require('string_decoder');
+// console.log({ stringDecoder: stringDecoder });
 
-// The server should responce to all the requests with a string 
-var server = http.createServer(function(req, res){
-    //Get the url and parse it.
-    const parsedUrl = url.parse(req.url, true)
+// The server should responce to all the requests with a string
+var server = http.createServer(function(req, res) {
+  //Get the url and parse it.
+  const parsedUrl = url.parse(req.url, true);
 
- //Get the path.
- const path = parsedUrl.pathname
- let truimmedPath = path.replace(/^\/+|\/+$/g,'')
- 
- //get the query string as an object
- let  queryStingObject = parsedUrl.query
- console.log({queryStingObject: queryStingObject})
- 
- 
-//  console.log(parsedUrl)
- 
- //Get the HTTP method
- const methoed = req.method.toLowerCase()
+  //Get the path.
+  const path = parsedUrl.pathname;
+  let truimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-//Get the header as an object
-const headers = req.headers
+  //get the query string as an object
+  let queryStingObject = parsedUrl.query;
+  // console.log({ queryStingObject: queryStingObject });
 
- // send the responce
- res.end('Hello world\n')
+  //  console.log(parsedUrl)
 
- // log the request path
- console.log(`Request received on path: 
-          ${truimmedPath}
-            with method
-          ${methoed}
-            with these query string parameters 
-          ${{queryStingObject}}
+  //Get the HTTP method
+  const methoed = req.method.toLowerCase();
 
-          request received on the headers = ${headers}
+  //Get the header as an object
+  const headers = req.headers;
+  //Get the payload if any
+  let decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end()
+    res.end("Hello world\n");
+    console.log( buffer);
+  });
 
-`, queryStingObject, headers)
-})
+});
+
+// send the responce
+
+// log the request path
+
 //Start the server and have it listen on port 777
-server.listen(3000, function(){
-    console.log('server is listening on port 3000 now')
-})
+server.listen(3000, function() {
+  console.log("server is listening on port 3000 now");
+});
